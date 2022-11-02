6374a2f624f22793af5f5e6b9e0c0e4c6ec69ca5
itsmeleonardo <carlosgamercovi20@gmail.com>
2022-11-01 19:56:36 -0500

    fix responsive for navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import 'styles/globals.css'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
-      <head>
+      <head lang="en">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Travel Booking</title>
       </head>
       <body>{children}</body>

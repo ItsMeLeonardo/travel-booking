5b8e2029b1aa17c28b74bb5aef61fac9c3084a05
itsmeleonardo <carlosgamercovi20@gmail.com>
2022-11-02 22:14:46 -0500

    complete "you saved" section

--- app/home/components/AccountActivity/index.tsx
+++ app/home/components/AccountActivity/index.tsx
@@ -27,6 +27,21 @@ const users = [
   },
 ]
 
+const saved = [
+  {
+    picture: 'https://i.pinimg.com/236x/11/dd/29/11dd29e31c4633a7fafbb1e0336a93bb.jpg',
+  },
+  {
+    picture: 'https://i.pinimg.com/564x/90/21/ba/9021ba02649c0904b14fbf94b5a9489c.jpg',
+  },
+  {
+    picture: 'https://i.pinimg.com/236x/d8/02/23/d802235866f2f70df3d122eec4a3f996.jpg',
+  },
+  {
+    picture: 'https://i.pinimg.com/236x/fe/e8/f6/fee8f6c0419006f12d27ee3812cb54c1.jpg',
+  },
+]
+
 export default function AccountActivity() {
   return (
     <div className={style.container}>
@@ -54,6 +69,16 @@ export default function AccountActivity() {
             </button>
           </div>
         ))}
+      </SeeAllSection>
+
+      <SeeAllSection title="You Saved" to="#">
+        <div className={style.savedGrid}>
+          {saved.map(({ picture }) => (
+            <button key={picture} className={style.image}>
+              <img src={picture} alt="" />
+            </button>
+          ))}
+        </div>
       </SeeAllSection>
     </div>
   )

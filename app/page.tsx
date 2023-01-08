66b592a08e25e970de3237db962a8ae1a1eb2ab4
itsmeleonardo <carlosgamercovi20@gmail.com>
2023-01-08 14:57:02 -0500

    fix index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import NextLink from 'next/link'
 
+import style from './style.module.css'
+
 export default function Index() {
   return (
-    <div>
-      <NextLink href="/home">Go home</NextLink>
+    <div className={style.container}>
+      <NextLink href="/home" className={style.button}>
+        Go Demo
+      </NextLink>
     </div>
   )
 }
